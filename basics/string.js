47a8b65c1c001dfb3ce6f556ef99179dca1642eb
unknown <truonghoaiphuonguyen@gmail.com>
2023-05-16 20:51:29 -0500

    add basics folder

---
+++ basics/string.js
@@ -0,0 +1,9 @@
+// initialize a var; use Camel case to name the var
+let firstName = 'Uyen'
+let lastName = 'Truong'
+let fullName = firstName + ' ' + lastName // concatenation operator
+
+console.log(firstName + ' ' + lastName)
+console.log(fullName)
+
+
