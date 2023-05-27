164e7b98fb7b6994e2212f20304b29f0103ca811
unknown <truonghoaiphuonguyen@gmail.com>
2023-05-27 09:17:16 -0500

    searching array

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1,113 +1,111 @@
-//----------------------------------------------
-// ARRAY BASICS
-const notes = ['Note 1', 'Note 2', 'Note 3', 'Note 4', 'Note 5']
-console.log(notes.length) //print out the # of array items
+// //----------------------------------------------
+// // ARRAY BASICS
+// const notes = ['Note 1', 'Note 2', 'Note 3', 'Note 4', 'Note 5']
+// console.log(notes.length) //print out the # of array items
 
-// List of ARRAY METHODS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-console.log(notes[0]) //index starts at 0 --> print out 'Note 1'
-console.log(notes[2]) // print out the last item- 'Note 3'
-console.log(notes[notes.length-1]) // also print out the last item- 'Note 3'
+// // List of ARRAY METHODS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+// console.log(notes[0]) //index starts at 0 --> print out 'Note 1'
+// console.log(notes[2]) // print out the last item- 'Note 3'
+// console.log(notes[notes.length-1]) // also print out the last item- 'Note 3'
 
-//----------------------------------------------
-// ARRAY METHODS
-// .push(newItem): Add a new item to the end of the array
-notes.push('My new note')
-console.log(notes) //new note added
+// //----------------------------------------------
+// // ARRAY METHODS
+// // .push(newItem): Add a new item to the end of the array
+// notes.push('My new note')
+// console.log(notes) //new note added
 
-// .pop(): Remove the last item in the array
-notes.pop()
-console.log(notes)
+// // .pop(): Remove the last item in the array
+// notes.pop()
+// console.log(notes)
 
-// .shift(): Remove the FIRST item in the array
-notes.shift() // 'Note 1' is removed
-console.log(notes)
+// // .shift(): Remove the FIRST item in the array
+// notes.shift() // 'Note 1' is removed
+// console.log(notes)
 
-// .unshift(newItem): Add a new item at the beginning of the array
-notes.unshift('My first note')
-console.log(notes)
+// // .unshift(newItem): Add a new item at the beginning of the array
+// notes.unshift('My first note')
+// console.log(notes)
 
-// .splice(index, # you want to remove): Remove item(s) from the middle of the array
-notes.splice(1, 2) //remove 2 items starting from index 1
-console.log(notes)
+// // .splice(index, # you want to remove): Remove item(s) from the middle of the array
+// notes.splice(1, 2) //remove 2 items starting from index 1
+// console.log(notes)
 
-// .splice(index, 1, newItem): Replace the item at the specied index with a new one
-notes.splice(2,1, 'This is the replaced item') //replace 'Note 5' at index 2 with the new item
-console.log(notes)
+// // .splice(index, 1, newItem): Replace the item at the specied index with a new one
+// notes.splice(2,1, 'This is the replaced item') //replace 'Note 5' at index 2 with the new item
+// console.log(notes)
 
-// .splice(index, 0, newItem): ADD a new item at the specified index
-notes.splice(1, 0, 'This is the added item') //add the new item at index 1
-console.log(notes)
+// // .splice(index, 0, newItem): ADD a new item at the specified index
+// notes.splice(1, 0, 'This is the added item') //add the new item at index 1
+// console.log(notes)
 
-// Change value of an item:
-notes[0] = 'Note 1'
-notes [1] = 'Note 2'
-notes[2] = 'Note 3'
-notes[3] = 'Note 4'
-console.log(notes)
+// // Change value of an item:
+// notes[0] = 'Note 1'
+// notes [1] = 'Note 2'
+// notes[2] = 'Note 3'
+// notes[3] = 'Note 4'
+// console.log(notes)
 
-//----------------------------------------------
-// .FOREACH() LOOP METHOD FOR ARRAYS
+// //----------------------------------------------
+// // .FOREACH() LOOP METHOD FOR ARRAYS
 
-// .forEach(function): loop thru the array and conduct the action for each of the items
+// // .forEach(function): loop thru the array and conduct the action for each of the items
 
-let testing = function(){
-    console.log('testing')
+// let testing = function(){
+//     console.log('testing')
 
-}
-notes.forEach(testing) // call function 'testing'
+// }
+// notes.forEach(testing) // call function 'testing'
 
-// define function directly in the .forEach()
-notes.forEach(function (){
-    console.log('testing 123')
-})
+// // define function directly in the .forEach()
+// notes.forEach(function (){
+//     console.log('testing 123')
+// })
 
-// pass function (item, index) {}: loop and take the item as well as the index of each item
-// Callback Function: function within a function
-notes.forEach (function (item, index) {
-    console.log(index)
-    console.log(item)
-})
+// // pass function (item, index) {}: loop and take the item as well as the index of each item
+// // Callback Function: function within a function
+// notes.forEach (function (item, index) {
+//     console.log(index)
+//     console.log(item)
+// })
 
-//----------------------------------------------
-// FOR LOOP STATEMENT
-// THREE parts of a For loop statement:
-    // 1. initializer
-    // 2. condition
-    // 3. final expression
+// //----------------------------------------------
+// // FOR LOOP STATEMENT
+// // THREE parts of a For loop statement:
+//     // 1. initializer
+//     // 2. condition
+//     // 3. final expression
 
-// Count forward 0-2
-// In C#: for (int i = 0; i <= 5; i++)
-for (let i = 0; i <= 2; i++) {
-    console.log(i)
-}
+// // Count forward 0-2
+// // In C#: for (int i = 0; i <= 5; i++)
+// for (let i = 0; i <= 2; i++) {
+//     console.log(i)
+// }
 
-// Count backward 2-0
-for (let i = 2; i >= 0; i--) {
-    console.log(i)
-}
+// // Count backward 2-0
+// for (let i = 2; i >= 0; i--) {
+//     console.log(i)
+// }
 
-// Count even numbers 0-10 only
-for (let i = 0; i <= 10; i = i + 2){
-    console.log(i)
-}
-// Equivalent to .forEach() method:
-// Print out first-last items of the array
-for (let count = 0; count < notes.length; count++){
-    console.log(notes[count])
-}
+// // Count even numbers 0-10 only
+// for (let i = 0; i <= 10; i = i + 2){
+//     console.log(i)
+// }
+// // Equivalent to .forEach() method:
+// // Print out first-last items of the array
+// for (let count = 0; count < notes.length; count++){
+//     console.log(notes[count])
+// }
 
-// Print out last-first items of the array
-for (let count = notes.length - 1; count >= 0; count--){
-    console.log(notes[count])
-}
+// // Print out last-first items of the array
+// for (let count = notes.length - 1; count >= 0; count--){
+//     console.log(notes[count])
+// }
 
 //----------------------------------------------
 // SEARCHING ARRAY
 
-// .indexOf(item): find the index for the specified item
-console.log(notes.indexOf('Note 2'))
-
 // Turn the item to individual objects:
+const notes = ['Note 1', 'Note 2', 'Note 3', 'Note 4', 'Note 5']
 const noteArr = [{
     title: 'My next trip',
     body: 'I would like to go to Spain'
@@ -118,6 +116,9 @@ const noteArr = [{
     title: 'Office modification',
     body: 'Get new seats'
 }]
+
+// .indexOf(item): find the index for the specified item
+console.log(notes.indexOf('Note 4'))
 
 // Search for an item that doesn't exist in the array:
 console.log(noteArr.indexOf({title: "Packing list", body: "Don't forget sunscreen"})) //output -1
@@ -133,11 +134,33 @@ let otherObject = {
 console.log(someObject === otherObject) // print out true/false
 
 // .findIndex(): allow passing in a function
-const index = noteArr.findIndex(function (note, index){
-    console.log(note)
-    return note.title === 'Habbits to work on'
-}) // print out the item up until the condition is true (= 'Habbits to work on')
+const index = noteArr.findIndex(function (item, index){
+    console.log(item) // stop printing when the condition below is true
+    return item.title === 'Habbits to work on' // index = the index of the 'Habbits to work on' item if it exists in the array
+})
 
+// A function to search the noteArr and find a specified note title using .findIndex()
+const findNote = function(noteArr, noteTitle){
+    const index = noteArr.findIndex(function (item, index) {
+        return item.title.toLowerCase() === noteTitle.toLowerCase() //return the index of the item that has the title passed in 'noteTitle', if not, return -1
+        // this return is for the callback function    
+    })
+    // this return print out the item at the found index
+    return noteArr[index]
+}
+
+// Equivalent functionality to findNote function- using .find() method
+const findNote2 = function(noteArr, noteTitle){
+    return noteArr.find( function (item, index) {
+        return item.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
+
+const note =  findNote(noteArr, 'Office modification')
+console.log(note)
+
+const note2 =  findNote2(noteArr, 'Office modification')
+console.log(note2)
 
 
 
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,27 +1,54 @@
-const todo = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
-console.log (`You have ${todo.length} todos!`)
-console.log(todo)
+// const todo = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
+// console.log (`You have ${todo.length} todos!`)
+// console.log(todo)
 
-// Delete the 3rd item:
-todo.splice(2,1)
+// // Delete the 3rd item:
+// todo.splice(2,1)
 
-// Add the new item onto the end
-todo.push('Do the dishes')
+// // Add the new item onto the end
+// todo.push('Do the dishes')
 
-// Remove the first item from the list
-todo.shift()
+// // Remove the first item from the list
+// todo.shift()
 
-console.log(todo)
+// console.log(todo)
 
-console.log (`You have ${todo.length} todos!`)
+// console.log (`You have ${todo.length} todos!`)
 
-// Loop thru the array and print out each item:
-todo.forEach(function(item, index){
-    console.log(`${index+1}. ${item}`)
-})
+// // Loop thru the array and print out each item:
+// todo.forEach(function(item, index){
+//     console.log(`${index+1}. ${item}`)
+// })
 
-// Using For Loop instead:
-for (let i = 0; i < todo.length; i++){
-    console.log(`${i+1}. ${todo[i]}`)
+// // Using For Loop instead:
+// for (let i = 0; i < todo.length; i++){
+//     console.log(`${i+1}. ${todo[i]}`)
+// }
+
+// 1. An array of objects --> text, completed
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+},{
+    text: 'Clean kitchen',
+    completed: false
+},{
+    text: 'Buy food',
+    completed: false
+},{
+    text: 'Do work',
+    completed: false
+},{
+    text: 'Exercise',
+    completed: false
+}]
+// 2. Create a function to remove a todo by text value
+const completeToDo = function(todos, text){
+    // find the index of the specified text in the array:
+    const index = todos.find( function (todo, index){
+        return todo.text === text
+    })
+    // change complete status to true
+    todos[index].completed = true
 }
 
