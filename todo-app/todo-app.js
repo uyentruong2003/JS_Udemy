9cff59a7f023a11e067d1aae7526dcd0b00b361f
unknown <truonghoaiphuonguyen@gmail.com>
2023-05-30 10:11:30 -0500

    advanced query

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -25,31 +25,58 @@ const todos = [{
 // })
 
 // Print a summary: you have 2 todos left
-const getSummary = function(todos){
-    let incompleteNum = 0
-    todos.forEach(function(todo){
-        if (!todo.completed){
-            incompleteNum ++
-        }
-    })
-    return `You have ${incompleteNum} todos left`
-}
+// const getSummary = function(todos){
+//     let incompleteNum = 0
+//     todos.forEach(function(todo){
+//         if (!todo.completed){
+//             incompleteNum ++
+//         }
+//     })
+//     return `You have ${incompleteNum} todos left`
+// }
 
-console.log(getSummary(todos))
+// Second way to filter out the incompleteTodos
+const incompleteTodos = todos.filter( function (todo) {
+    return !todo.completed
+})
 //(1) create element
 const summary = document.createElement('p')
 //(2) create content
-summary.textContent = getSummary(todos)
+summary.textContent = `You have ${incompleteTodos.length} todos left:`
 //(3) append to the body tag
 document.querySelector('body').appendChild(summary)
 
+// Sort the arrays:
+const sortTodos = function (todos){
+    const sorted = todos.sort(function (a,b){
+        if (!a.completed && b.completed){
+            return -1
+        } else if (a.completed && !b.completed){
+            return 1
+        } else{
+            return 0
+        }
+    })
+}
 
+sortTodos(todos)
+console.log(todos)
 // Add a p for each todo above
 todos.forEach(function(todo){
     //(1) create element
-    const newParagraph = document.createElement('p')
+    const p = document.createElement('p')
     //(2) create content
-    newParagraph.textContent = `${todo.text} - complete status: ${completed}`
+    if (!todo.completed){
+        p.textContent = `${todo.text} - incomplete`
+    } else {
+        p.textContent = `${todo.text} - needs action`
+    }
+    
     //(3) append to the body tag
-    document.querySelector('body').appendChild(newParagraph)
+    document.querySelector('body').appendChild(p)
+})
+
+//Listen for new todo creation
+document.querySelector('#add-todo').addEventListener('click', function (e){
+    console.log('Add a new todo...')
 })
